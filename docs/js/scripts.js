3a5ff5b81832901ea247a4a9f58ceadf6592947e
Damita Salmon <damita.salmon@gmail.com>
2020-11-14 18:44:25 -0500

    Update build to push .jpg imgs

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -5,30 +5,6 @@
     */
     (function ($) {
     "use strict"; // Start of use strict
-    // remove white around goFundMe
-        /*
-          1. Inject CSS which makes iframe invisible
-      */
-    
-     var div = document.createElement('div'),
-     ref = document.getElementsByTagName('base')[0] || 
-           document.getElementsByTagName('script')[0];
-
-    div.innerHTML = '&shy;<style> iframe { visibility: hidden; } </style>';
-
-    ref.parentNode.insertBefore(div, ref);
-
-        
-    /*
-        2. When window loads, remove that CSS, 
-            making iframe visible again
-    */
-    
-    window.onload = function() {
-        div.parentNode.removeChild(div);
-    }
- 
-
 
     // Smooth scrolling using jQuery easing
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
